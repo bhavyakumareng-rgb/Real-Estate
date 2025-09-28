c935be909d748d024791420da1f8743e7c0335f7
Bhavya Kumar <bhavyakumar@Bhavyas-MacBook-Air.local>
2025-09-28 15:52:24 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",       // for plain HTML files
+    "./src/**/*.{js,jsx,ts,tsx}"  // for React components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
